f25532435e576e65a534ae845843119694e7f052
maccrin <maccrin@gmail.com>
2024-01-04 14:27:31 +0100

    final version

--- form-validation/script.js
+++ form-validation/script.js
@@ -4,13 +4,46 @@ const pwd2 = document.getElementById('password2');
 const msgContainer = document.querySelector('.message-container');
 const msg = document.getElementById('message')
 let isValid = false;
+let passwordConfirm = false;
 const validateForm = () => {
     isValid = form.checkValidity();
-    msg.innerHTML = 'Please fill all the fields';
-    msg.style.color = 'red';
+    if (!isValid) {
+        msg.innerHTML = 'Please fill all the fields';
+        msg.style.color = 'red';
+        return
+    }
+    if (pwd1.value === pwd2.value) {
+        passwordConfirm = true;
+        pwd1.style.borderColor = 'green';
+        pwd2.style.borderColor = 'green';
+        msg.style.color = 'green';
+        msg.innerHTML = 'Passowrd match!!';
+    }
+    else {
+        msg.innerHTML = 'Passowrd doesn\'t match';
+        msg.style.color = 'red';
+        pwd1.style.borderColor = 'red';
+        pwd2.style.borderColor = 'red';
+        return;
+
+    }
+    if (isValid && passwordConfirm) {
+        msg.style.color = 'green'
+        msg.innerHTML = 'Succefully Registered!';
+    }
+}
+function storeFormData() {
+    user = {
+        name: form.name.value,
+        phone: form.phone.value,
+        email: form.email.value,
+        website: form.website.value,
+        password: form.password.value
+    }
 }
 const submitData = (event) => {
     event.preventDefault();
     validateForm();
+    if (isValid && passwordConfirm) storeFormData()
 }
 form.addEventListener('submit', submitData);
